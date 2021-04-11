90422deeda0d2efedca12b8e4b5fa3ab53760da6
danielfeather <danielfeather@users.noreply.github.com>
2021-04-11 18:38:34 +0100

    Switched to Vue CLI to build library

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import DfSelect from "./components/atoms/DfSelect/DfSelect.vue";
 import DfButton from "./components/atoms/DfButton/DfButton.vue";
 import DfTag from "./components/atoms/DfTag/DfTag.vue";
 
-import "/src/assets/css/main.css";
-
 export {
   DfSelect,
   DfButton,
